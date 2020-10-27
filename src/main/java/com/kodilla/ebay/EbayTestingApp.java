f8431625f322b30dee5ff04280e97e7914cf0242
zagPatryk <zag.patryk@gmail.com>
2020-10-27 20:36:53 +0100

    Ebay simple test

---
+++ src/main/java/com/kodilla/ebay/EbayTestingApp.java
@@ -0,0 +1,19 @@
+package com.kodilla.ebay;
+
+import com.kodilla.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class EbayTestingApp {
+    public static final String SEARCHFIELD = "_nkw";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com/");
+
+        WebElement searchField = driver.findElement(By.name(SEARCHFIELD));
+        searchField.sendKeys("Laptop");
+        searchField.submit();
+    }
+}
